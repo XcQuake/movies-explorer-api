45a644cc589af2e3eca62d1fe378ed908af2e1ec
XcQuid <creed.35@yandex.ru>
2022-05-24 13:09:52 +0300

    feat: add movie and user models

---
+++ models/movie.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const urlValidator = require('../../express-mesto/validators/urlValidator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: urlValidator,
+  },
+  owner: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
