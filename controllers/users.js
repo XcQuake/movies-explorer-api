61d41de83398435caf061a9d1a07447be572a7ec
XcQuid <creed.35@yandex.ru>
2022-05-24 15:10:17 +0300

    feat: add auth controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,12 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const ConflictError = require('../../express-mesto/errors/ConflictError');
 const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
 
 const User = require('../models/user');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.getProfile = (req, res, next) => {
   User.findById(req.user._id)
@@ -25,4 +28,51 @@ module.exports.updateProfile = (req, res, next) => {
         next(err);
       }
     });
+};
+
+module.exports.createProfile = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      email: req.body.email,
+      password: hash,
+      name: req.body.name,
+    }))
+    .then((user) => res.status(201).send({
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при создании пользователя.'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Пользователь с этой почтой уже зарегистрирован'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.signIn = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        secure: true,
+      }).send({ message: 'Успешный вход в систему' });
+    })
+    .catch(next);
+};
+
+module.exports.signOut = (req, res) => {
+  res.clearCookie('jwt').send({ message: 'Успешный выход из системы' });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const auth = require('../middlewares/auth');
+const { createProfile, signIn, signOut } = require('../controllers/users');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const NotFoundError = require('../errors/NotFoundError');
 
+router.use('/signup', createProfile);
+router.use('/signin', signIn);
+
+router.use(auth);
+
+router.post('/signout', signOut);
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
 
