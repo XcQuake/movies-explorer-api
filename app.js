a3097d43130c3d19f1acb6c6987e786bb5a85f45
XcQuid <creed.35@yandex.ru>
2022-06-17 10:07:49 +0300

    feat: add cors allowed origins

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { NODE_ENV, DB_ADDRESS, PORT = 3000 } = process.env;
+const whiteList = [
+  'http://localhost:3000',
+  'http://xcqmovies.nomoredomains.xyz/',
+  'https://xcqmovies.nomoredomains.xyz/',
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
@@ -25,7 +39,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(router);
 app.use(errorLogger);
